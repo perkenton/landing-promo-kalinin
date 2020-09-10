9cf917afd1ec16f9b96519e0f762b607757ca3e2
Alexander Vasiliev <selado@ya.ru>
2020-09-10 18:48:39 +0300

    fix image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -51,10 +51,6 @@ module.exports = {
           {
             loader: 'image-webpack-loader',
             options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
               // optipng.enabled: false will disable optipng
               optipng: {
                 enabled: false,
