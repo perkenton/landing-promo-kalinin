f2174000104665da34e66fbba2459d07bb5f929b
Alexander Vasiliev <selado@ya.ru>
2020-09-10 03:23:09 +0300

    add two sections and two sliders other

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,36 +1,39 @@
 import '../pages/index.css';
 
 import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
 
 'use strict';
 
 (function() {
 
-  const swiperContainer = document.querySelector('.swiper__container');
+  const swiperContainers = document.querySelectorAll('.swiper__container');
 
-  Swiper.use([Navigation, Pagination]);
-  const swiper = new Swiper(swiperContainer, {
-    init: true,
-    slidesPerView: 'auto',
-    setWrapperSize: true,
-    grabCursor: true,
-    loop: true,
-    spaceBetween: 15,
-    wrapperClass: 'swiper__wrapper',
-    slideClass: 'swiper__image',
+  swiperContainers.forEach((el) => {
 
-    pagination: {
-      el: '.swiper__pagination-container',
-      type: 'bullets',
-      clickable: true,
-      bulletClass: 'swiper__pagination-item',
-      bulletActiveClass: 'swiper__pagination-item_is-active',
-    },
+    new Swiper(el, {
+      init: true,
+      slidesPerView: 'auto',
+      setWrapperSize: true,
+      grabCursor: true,
+      loop: true,
+      spaceBetween: 15,
+      wrapperClass: 'swiper__wrapper',
+      slideClass: 'swiper__image',
 
-    navigation: {
-      nextEl: '.swiper__arrow-next',
-      prevEl: '.swiper__arrow-prev',
-    },
+      pagination: {
+        el: el.querySelector('.swiper__pagination-container'),
+        type: 'bullets',
+        clickable: true,
+        bulletClass: 'swiper__pagination-item',
+        bulletActiveClass: 'swiper__pagination-item_is-active',
+      },
+
+      navigation: {
+        nextEl: el.querySelector('.swiper__arrow-next'),
+        prevEl: el.querySelector('.swiper__arrow-prev'),
+      },
+    });
   });
 
 })();
